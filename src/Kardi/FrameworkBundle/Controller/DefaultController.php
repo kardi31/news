c24be75869b54ee0fd51fccba41cf3f3baad35f8
Tomasz Kardas <tomasz@usemarkup.com>
2017-03-03 15:22:12 +0000

    Nowe ikony fb

--- src/Kardi/FrameworkBundle/Controller/DefaultController.php
+++ src/Kardi/FrameworkBundle/Controller/DefaultController.php
@@ -12,13 +12,6 @@ class DefaultController extends Controller
         $settings = $em->getRepository('KardiFrameworkBundle:Settings')
                 ->getSettings();
 
-        $facebookService = $this->container->get('kardi.framework.facebook_service');
-//var_dump($settings->getFacebook());exit;
-//        $facebookId = $settings->getFacebookId();
-
-        $facebookId = 371309372997823;
-        $facebookFollowers = $facebookService->getNumberOfFollowers($facebookId);
-
         return $this->render('KardiFrameworkBundle:Default:social-numbers.html.twig', ['settings' => $settings]);
     }
 }
